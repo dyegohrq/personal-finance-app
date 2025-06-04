0c5bdaa66efa51dfc6991e5bb9826619f51c7dad
José Dyego Henrique Cardoso de Sousa <116648398+dyegohrq@users.noreply.github.com>
2025-06-04 10:49:49 -0300

    Update router.tsx

--- src/router.tsx
+++ src/router.tsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter } from "react-router";
-import Layout  from "./components/Layout";
+import Layout  from "./components/layout";
 import { Home } from "./pages/Home";
 import { Budgets } from "./pages/Budgets";
 import { Pots } from "./pages/Pots";
